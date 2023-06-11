4468f26bd5aa1c847ca5a7c21115d4e9bfa80308
phtrebil <phptrebil@gmail.com>
2023-06-10 21:04:46 -0300

    adicionando conversor de json

--- app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++ app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -2,11 +2,13 @@ package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.service.NotaService
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInicializador {
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("http://192.168.0.1:8080/")
+        .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     val notaService = retrofit.create(NotaService::class.java)

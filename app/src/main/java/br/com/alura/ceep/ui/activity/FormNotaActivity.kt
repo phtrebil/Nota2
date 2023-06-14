b308a9c7f8cd96e2f29d1478b6b84252a8eb7d05
phtrebil <phptrebil@gmail.com>
2023-06-14 17:41:43 -0300

    implementando UUID e sua migration

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormNotaActivity.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormNotaActivity.kt
@@ -1,7 +1,6 @@
 package br.com.alura.ceep.ui.activity
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View.GONE
@@ -26,7 +25,7 @@ class FormNotaActivity : AppCompatActivity() {
     private val dao by lazy {
         AppDatabase.instancia(this).notaDao()
     }
-    private var notaId: Long = 0L
+    private var notaId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,18 +58,21 @@ class FormNotaActivity : AppCompatActivity() {
     }
 
     private suspend fun tentaBuscarNota() {
-        dao.buscaPorId(notaId)
-            .filterNotNull()
-            .collect { notaEncontrada ->
-                notaId = notaEncontrada.id
-                imagem.value = notaEncontrada.imagem
-                binding.activityFormNotaTitulo.setText(notaEncontrada.titulo)
-                binding.activityFormNotaDescricao.setText(notaEncontrada.descricao)
-            }
+        notaId?.let { id ->
+            dao.buscaPorId(id)
+                .filterNotNull()
+                .collect { notaEncontrada ->
+                    notaId = notaEncontrada.id
+                    imagem.value = notaEncontrada.imagem
+                    binding.activityFormNotaTitulo.setText(notaEncontrada.titulo)
+                    binding.activityFormNotaDescricao.setText(notaEncontrada.descricao)
+                }
+        }
+
     }
 
     private fun tentaCarregarIdDaNota() {
-        notaId = intent.getLongExtra(NOTA_ID, 0L)
+        notaId = intent.getStringExtra(NOTA_ID)
     }
 
     private fun configuraImagem() {
@@ -103,7 +105,9 @@ class FormNotaActivity : AppCompatActivity() {
 
     private fun remove() {
         lifecycleScope.launch {
-            dao.remove(notaId)
+            notaId?.let { id ->
+                dao.remove(id)
+            }
             finish()
         }
     }
@@ -119,7 +123,14 @@ class FormNotaActivity : AppCompatActivity() {
     private fun criaNota(): Nota {
         val titulo = binding.activityFormNotaTitulo.text.toString()
         val descricao = binding.activityFormNotaDescricao.text.toString()
-        return Nota(
+        return notaId?.let { id ->
+            Nota(
+                id = id,
+                titulo = titulo,
+                descricao = descricao,
+                imagem = imagem.value
+            )
+        } ?: Nota(
             id = notaId,
             titulo = titulo,
             descricao = descricao,
